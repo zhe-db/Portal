c5f8440bd434d73216e028ec7cc29081db1a116b
walt <walt.wang@appdirect.com>
2017-10-05 04:52:13 -0400

    make changes to angular cli to make defalut base href run for electron

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600})
-
+  win.setMenu(null);
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, '/dist/index.html'),
