3cd0846208fcac8a38a677ba141613bb01b5c3e6
walt <walt.wang@appdirect.com>
2017-10-06 05:27:25 -0400

    add MainMenu file setup, ng-bootstrap, update angular cli to latest version, angular/core, angular/common to latest version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './MainMenu/MainMenu.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
